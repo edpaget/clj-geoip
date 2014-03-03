45b2116ccb818f1a6eb0f11fe2f29d2c785db263
Edward Paget <ed.paget@gmail.com>
2014-03-03 12:40:09 +0800

    Make it possible to access files inside jar

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zmedelis/clj-geoip "0.1"
+(defproject zmedelis/clj-geoip "0.1-SNAPSHOT"
   :description "Thin Clojure layer on top of the Java GeoIP API.
 Please have a look at the GeoIP homepage at http://www.maxmind.com/app/ip-location."
   :url "https://github.com/Norrit/clj-geoip"
@@ -9,7 +9,9 @@ Please have a look at the GeoIP homepage at http://www.maxmind.com/app/ip-locati
   :resource-paths ["resources"]
   :min-lein-version "2.0.0"
   :aot :all
+  :main clj-geoip.core
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [commons-io "2.4"]
                  [com.maxmind.geoip/geoip-api "1.2.11"]]
-  :dev-dependencies [[lein-marginalia "0.7.0"]]
-  :plugins [[lein-swank "1.4.4"]])
+  :profiles {:dev
+             {:dependencies [[lein-marginalia "0.7.0"]]}})
--- src/clj_geoip/core.clj
+++ src/clj_geoip/core.clj
@@ -3,19 +3,45 @@
   Use `geoip-init` and `geoip-close` to start and stop the service and `lookup` to
   lookup information about the given IP."
   (:require [clojure.java.io :refer :all])
-  (:import [com.maxmind.geoip LookupService]))
+  (:import [com.maxmind.geoip LookupService]
+           [org.apache.commons.io FileUtils])
+  (:gen-class :main true))
 
-;; Setup
-;; ============
+(defn- lookup-location
+  "Looks up IP location information."
+  [{:keys [city]}]
+  (fn [ip] 
+    (if-let [location (.getLocation city ip)]
+      {:ip ip
+       :countryCode (.countryCode location)
+       :countryName (.countryName location)
+       :region (.region location)
+       :city (.city location)
+       :postalCode (.postalCode location)
+       :latitude (.latitude location)
+       :longitude (.longitude location)
+       :dma-code (.dma_code location)
+       :area-code (.area_code location)
+       :metro-code (.metro_code location)})))
 
-;; They should be private so no one messes around with them.
-(def ^{:private true} geoip-city (ref nil))
-(def ^{:private true} geoip-asn (ref nil))
+(defn- lookup-asn
+  "Looks up IP provider information."
+  [{:keys [asn]}]
+  (fn [ip]
+    (if-let [asn (.getOrg asn ip)]
+      {:ip ip
+       :asn asn})))
 
-(defn get-dbs
-  []
-  {:city (-> "./GeoLiteCity.dat" resource .toURI java.io.File.)
-   :asn  (-> "./GeoIPASNum.dat" resource .toURI java.io.File.)})
+(defn lookup
+  "Looks up all available IP information."
+  [dbs]
+  (let [loc (lookup-location dbs)
+        asn (lookup-asn dbs)] 
+    (fn [ip] 
+      (if-let [geoinfo (merge (loc ip)
+                              (asn ip))]
+        geoinfo
+        {:error "IP not localized"}))))
 
 (defn- geoip-mode
   "Looks up the matching mode to the given keyword."
@@ -31,80 +57,20 @@
   [db mode]
   (if mode
     (LookupService. db (geoip-mode mode))
-    (LookupService. db)))
+    (LookupService. db))) 
+
+(defn get-db
+  [db-name mode]
+  (let [url (resource db-name)
+        f (as-file (str "/tmp/" db-name))]
+    (FileUtils/copyURLToFile url f)
+    (geoip-init-db f mode)))
 
 (defn geoip-init
   "Initializes the GeoIP service.
-  The modes `:memory`, `:check` or `:index` are possible."
-  [& [dbfn mode]]
-  (dosync
-    (let [dbfn (or dbfn get-dbs)
-          dbs (dbfn)
-          city (geoip-init-db (:city dbs) mode)
-          asn (geoip-init-db (:asn dbs) mode)]
-      (ref-set geoip-city city)
-      (ref-set geoip-asn asn)
-      true)))
- 
-(defn geoip-close
-  "Shuts down the GeoIP service."
-  []
-  (dosync
-    (.close @geoip-asn)
-    (ref-set geoip-asn nil)
-    (.close @geoip-city)
-    (ref-set geoip-city nil)
-    true))
-
-
-;; Helper
-;; ============
-
-(defn initialized?
-  "Checks whether the GeoIP service is initialized or not."
-  []
-  (and (not (nil? @geoip-city)) (not (nil? @geoip-asn))))
-
-(defmacro with-init-check
-  "Wrapps the given statements with an init check."
-  [body]
-  `(if (initialized?)
-     ~body
-     (throw (IllegalStateException. "GeoIP not initialized."))))
-
-
-;; Lookup
-;; =============
-
-(defn- lookup-location
-  "Looks up IP location information."
-  [ip]
-  (with-init-check 
-    (if-let [location (.getLocation @geoip-city ip)]
-      {:ip ip
-       :countryCode (.countryCode location)
-       :countryName (.countryName location)
-       :region (.region location)
-       :city (.city location)
-       :postalCode (.postalCode location)
-       :latitude (.latitude location)
-       :longitude (.longitude location)
-       :dma-code (.dma_code location)
-       :area-code (.area_code location)
-       :metro-code (.metro_code location)})))
-
-(defn- lookup-asn
-  "Looks up IP provider information."
-  [ip]
-  (with-init-check
-    (if-let [asn (.getOrg @geoip-asn ip)]
-      {:ip ip
-       :asn asn})))
-
-(defn lookup
-  "Looks up all available IP information."
-  [ip]
-  (if-let [geoinfo (merge (lookup-location ip)
-                          (lookup-asn ip))]
-    geoinfo
-    {:error "IP not localized"}))
+  The modes `:memory`, `:check` or `:index` are possible.
+  Returns the Lookup function"
+  [& {:keys [city-db asn-db mode] :or {mode :memory}}]
+  (with-open [city (get-db (or city-db "GeoLiteCity.dat") mode)
+              asn (get-db (or asn-db "GeoIPASNum.dat") mode)]
+    (lookup {:city city :asn asn})))
